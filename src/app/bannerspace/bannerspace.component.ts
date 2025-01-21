dcef2beb5ed493a77dbc4d064b36ca5949f649c8
Serwaa <mac@SERWAA.local>
2025-01-21 17:02:15 +0000

    first page created

---
+++ src/app/bannerspace/bannerspace.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bannerspace',
+  templateUrl: './bannerspace.component.html',
+  styleUrls: ['./bannerspace.component.scss']
+})
+export class BannerspaceComponent {
+
+}
