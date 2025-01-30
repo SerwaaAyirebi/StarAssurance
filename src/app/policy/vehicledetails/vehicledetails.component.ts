72b68c73d99dae4294d926cc588175a8b15f0e35
Serwaa <mac@SERWAA.local>
2025-01-30 09:38:52 +0000

    completed

--- src/app/policy/vehicledetails/vehicledetails.component.ts
+++ src/app/policy/vehicledetails/vehicledetails.component.ts
@@ -1,9 +1,28 @@
 
 import { Component, TemplateRef, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, NgForm } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import {ThemePalette} from '@angular/material/core';
 import { ProgressBarMode } from '@angular/material/progress-bar';
+
+
+export class vehicledetails {
+    registrationYear!: number;
+    manufactureYear!: number;
+    vehicleUsage!:string;
+    vehicleType!:string;
+    registrationNumber!:number;
+    passengerCount!:number;
+    vehicleColor!:string;
+    vehicleValue!:number;
+    chassisNumber!:number;
+    vehicleMake!:string;
+    vehicleModel!:string;
+    tppdAmount!:number;
+    cubicCapacity!:number;
+    buyExcess!:number;
+  }
+
 
 @Component({
   selector: 'app-vehicledetails',
@@ -11,13 +30,100 @@ import { ProgressBarMode } from '@angular/material/progress-bar';
   styleUrls: ['./vehicledetails.component.scss']
 })
 export class VehicledetailsComponent {
-    color: ThemePalette = 'primary';
-    mode: ProgressBarMode = 'determinate';
-    value = 66.66;
-    bufferValue = 75;
-    determinate!: ProgressBarMode;
+  
+form :vehicledetails;
+formList = [];
+
+// formList: any;
+
+
+createNewVehicleForm(){
+  return{
+          registrationYear: '',
+          manufactureYear: '',
+           vehicleUsage:'',
+           vehicleType:'',
+           registrationNumber:'',
+           passengerCount:'',
+           vehicleColor:'',
+           vehicleValue:'',
+           chassisNumber:'',
+           vehicleMake:'',
+           vehicleModel:'',
+           tppdAmount:'',
+           cubicCapacity:'',
+           buyExcess:''
+  }
+}
+
+constructor(){
+  this.form = new vehicledetails()
+}
+
+  onSubmit(form:NgForm){
+    console.log("hi");
   }
 
+addForm(form:vehicledetails){
+  this.formList.push();
+  console.log("hello")
+}
+
+removeItem(){
+  console.log("hey",this.form)
+}
+
+}
 
 
+
+
+
+
+
+
+  
+
+// let forms: vehicledetails[] =[
+
+//     {
+//     }
+// ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //   this.createNewForm() 
+  // ];
+
+
+//    createNewForm() {
+
+//     return{
+//       
+//     };
+//   }
+
+//   addForm(){
+//     this.form.push(this.createNewForm());
+//   }
+
+//   submitForms(){
+//     console.log(this.form)
+//   }
+//   }
+
+// removeForm(){
+
+// }
 
